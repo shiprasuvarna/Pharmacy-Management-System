5d07e33b98d0c0be07b76774108293ac0f75d002
Shipra Suvarna <shipraasuvarna@gmail.com>
2024-01-07 13:09:21 +0530

    Add files via upload

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+/**
+ *
+ * @author Shipra
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy?useSSL=false","root","root");
+            return con;
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return null;
+                }
+    }
+}
---
+++ src/dao/PharmacyUtils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author Shipra
+ */
+public class PharmacyUtils {
+    public static String billPath = "C:\\Users\\Shipra\\OneDrive\\Documents\\NetBeansProjects\\Pharmacy Management\\src\\Bills\\";
+}
