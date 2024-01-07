21d6256d7142f71beb85a42a73dd568fa8353b1a
Shipra Suvarna <shipraasuvarna@gmail.com>
2024-01-07 13:04:42 +0530

    Add files via upload

---
+++ src/jTable1.java
@@ -0,0 +1,24 @@
+
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shipra
+ */
+class jTable1 {
+
+    static int getSelectedRow() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static TableModel getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
