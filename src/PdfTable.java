8099f27bd84238aa0946e6144a282feb752d6949
Shipra Suvarna <shipraasuvarna@gmail.com>
2024-01-07 13:00:27 +0530

    Add files via upload

---
+++ src/PdfTable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shipra
+ */
+class PdfTable {
+
+    PdfTable(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addCell(String medicine_ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
